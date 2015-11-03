903389e3af5d8b56f1faba3500727f3f0b1730dc
Yulong Tan <swifties@uw.edu>
2015-11-03 01:40:37 -0800

    converted EditText to Strings and set up the startApp method

--- app/src/main/java/yulongproductions/com/quickplay/MainActivity.java
+++ app/src/main/java/yulongproductions/com/quickplay/MainActivity.java
@@ -1,9 +1,11 @@
 package yulongproductions.com.quickplay;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -26,5 +28,25 @@ public class MainActivity extends ActionBarActivity {
         this.loginButton = (Button) findViewById(R.id.loginButton);
         this.mGlobalFunctions = new GlobalFunctions(this);
         this.mGlobalFunctions.setupUI(findViewById(R.id.mainLayout));
+
+        final String username = this.convert(this.username);
+        final String password = this.convert(this.username);
+
+        this.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startApp(username, password);
+            }
+        });
+    }
+
+    private void startApp(String username, String password) {
+        Intent intent = new Intent();
+        intent.putExtra("username", username);
+        intent.putExtra("password", password);
+    }
+
+    private String convert(EditText e) {
+        return e.getText().toString();
     }
 }
