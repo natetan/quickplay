aa603c8b9dffbc5226d4657c379781bfe83fd4a7
Yulong Tan <swifties@uw.edu>
2015-11-23 01:26:02 -0800

    linked Tabs to TabHost

--- app/src/main/java/yulongproductions/com/quickplay/FindGameActivity.java
+++ app/src/main/java/yulongproductions/com/quickplay/FindGameActivity.java
@@ -1,30 +1,44 @@
 package yulongproductions.com.quickplay;
 
 import android.app.ListActivity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.TabHost;
 
 
-public class FindGameActivity extends ListActivity {
+public class FindGameActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_game);
 
-        String[] currentGames = {
-                "Basketball",
-                "Volleyball",
-                "Soccer",
-                "Baseball",
-                "Football"
-        };
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, currentGames);
-        setListAdapter(adapter);
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("First Tab");
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("Second Tab");
+        TabHost.TabSpec tab3 = tabHost.newTabSpec("Third Tab");
+
+        // Set the Tab name and Activity
+        // that will be opened when particular Tab will be selected
+        tab1.setIndicator("Games");
+        tab1.setContent(new Intent(this,ListSegment.class));
+
+        tab2.setIndicator("Create");
+        tab2.setContent(new Intent(this,CreateSegment.class));
+
+        tab3.setIndicator("Profile");
+        tab3.setContent(new Intent(this,ProfileSegment.class));
+
+        /** Add the tabs  to the TabHost to display. */
+        tabHost.addTab(tab1);
+        tabHost.addTab(tab2);
+        tabHost.addTab(tab3);
+
     }
 }
