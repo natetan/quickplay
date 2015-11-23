236125655d82e3adb07e888de225aedb2131df35
Yulong Tan <swifties@uw.edu>
2015-11-23 00:26:32 -0800

    created ListActivity for find game activity

--- app/src/main/java/yulongproductions/com/quickplay/FindGameActivity.java
+++ app/src/main/java/yulongproductions/com/quickplay/FindGameActivity.java
@@ -1,38 +1,30 @@
 package yulongproductions.com.quickplay;
 
+import android.app.ListActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 
-public class FindGameActivity extends ActionBarActivity {
+public class FindGameActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_game);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_find_game, menu);
-        return true;
-    }
+        String[] currentGames = {
+                "Basketball",
+                "Volleyball",
+                "Soccer",
+                "Baseball",
+                "Football"
+        };
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, currentGames);
+        setListAdapter(adapter);
     }
 }
