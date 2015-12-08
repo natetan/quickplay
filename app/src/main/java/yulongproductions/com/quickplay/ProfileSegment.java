88892410ebc5f6bab2bca36e8ba0d0bab8d462f1
Yulong Tan <swifties@uw.edu>
2015-12-08 03:48:53 -0800

    created profile segment and fixed array bounds bug

--- app/src/main/java/yulongproductions/com/quickplay/ProfileSegment.java
+++ app/src/main/java/yulongproductions/com/quickplay/ProfileSegment.java
@@ -1,38 +1,50 @@
 package yulongproductions.com.quickplay;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class ProfileSegment extends ActionBarActivity {
+
+    private TextView statsTextView;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_segment);
+        getSupportActionBar().hide();
+
+        Intent intent = getIntent();
+        this.username = intent.getStringExtra(getString(R.string.username));
+        int numbers[] = this.getNumbers();
+
+        statsTextView = (TextView) findViewById(R.id.statTextView);
+        statsTextView.setText(
+                "Username: " + this.username +
+                        "\nSkill: Advanced" +
+                        "\nFavorite Sport: Basketball" +
+                        "\nGames Played: " + numbers[0] +
+                        "\nGames Created: " + numbers[1] +
+                        "\nGames Joined: " + numbers[2]
+
+        );
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_profile_segment, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private int[] getNumbers() {
+        int[] list = new int[3];
+        Random r = new Random();
+        list[0] = r.nextInt(31) + 1;
+        list[1] = r.nextInt(list[0]);
+        list[2] = list[0] - list[1];
+        return list;
     }
 }
