d08fd76f7cc43cbb6245da9b7dc9276b144dc662
Yulong Tan <swifties@uw.edu>
2015-12-08 03:48:18 -0800

    hid action bar

--- app/src/main/java/yulongproductions/com/quickplay/MainActivity.java
+++ app/src/main/java/yulongproductions/com/quickplay/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
 
         this.mNameText = (EditText) findViewById(R.id.nameEditText);
         this.mPassword = (EditText) findViewById(R.id.passwordEditText);
